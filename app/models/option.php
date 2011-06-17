f9aa415deec6024c53d538226259342bff2ec663
Xudong Wu <william.wu.xudong@gmail.com>
2011-06-17 09:10:15 +0800

    redesign of survey module

--- app/models/option.php
+++ app/models/option.php
@@ -3,6 +3,24 @@ class Option extends AppModel {
 	var $name = 'EsOption';
 	var $belongsTo = array ('Question' );
 	var $parent_name = array ('Question' );
+	var $hasAndBelongsToMany = array(
+        'Answer' =>
+            array(
+                'className'              => 'Answer',
+                'joinTable'              => 'answers_options',
+                'foreignKey'             => 'option_id',
+                'associationForeignKey'  => 'answer_id',
+                'unique'                 => true,
+                'conditions'             => '',
+                'fields'                 => '',
+                'order'                  => '',
+                'limit'                  => '',
+                'offset'                 => '',
+                'finderQuery'            => '',
+                'deleteQuery'            => '',
+                'insertQuery'            => ''
+            )
+    );
 	
 	public $validate = array ('name' => array ('rule' => 'notEmpty', 'message' => 'SubCategory Name is required' ));
 }
