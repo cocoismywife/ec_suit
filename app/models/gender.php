d089e577971358b667574886e02b7732f5f5c8bd
Xudong Wu <william.wu.xudong@gmail.com>
2011-07-23 20:50:54 +0800

    support for two image upload for genders
    Signed-off-by: Xudong Wu <william.wu.xudong@gmail.com>

--- app/models/gender.php
+++ app/models/gender.php
@@ -1,8 +1,14 @@
 <?php
 class Gender extends AppModel {
-	var $name = 'EsGenders';
-	
-	var $validate = array ('name' => array ('rule' => 'notEmpty', 'message' => 'Gender Name is required' ) );
-	var $belongsTo = array ('Image' => array ('className' => 'Image', 'foreignKey' => 'image_id' ) );
-	var $parent_name = array ('Image' );
+    var $name = 'EsGenders';
+    
+    var $validate = array(
+        'name' => array(
+        'rule' => 'notEmpty', 'message' => 'Gender Name is required'));
+    var $belongsTo = array(
+        'Image' => array(
+        'className' => 'Image', 'foreignKey' => 'image_id'), 'ImageMirror' => array(
+        'className' => 'ImageMirror', 'foreignKey' => 'image_mirror_id'));
+    var $parent_name = array(
+        'Image', 'ImageMirror');
 }
