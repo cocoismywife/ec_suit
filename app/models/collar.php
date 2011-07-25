16272e239f6a17de7f026cf98d19974720b534ed
Xudong Wu <william.wu.xudong@gmail.com>
2011-07-26 00:07:57 +0800

    fix the view display error
    Signed-off-by: Xudong Wu <william.wu.xudong@gmail.com>

--- app/models/collar.php
+++ app/models/collar.php
@@ -4,8 +4,7 @@ class Collar extends AppModel {
     
     var $belongsTo = array(
         'Image' => array(
-        'className' => 'Image', 'foreignKey' => 'image_id'), 'ImageMirror' => array(
-        'className' => 'ImageMirror', 'foreignKey' => 'image_mirror_id'));
+        'className' => 'Image', 'foreignKey' => 'image_id'));
     var $parent_name = array(
-        'Image', 'ImageMirror');
+        'Image');
 }
