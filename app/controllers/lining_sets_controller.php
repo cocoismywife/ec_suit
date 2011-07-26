1c6cdde6a124f36b3e332a0c9d3c513f0e2543d4
Xudong Wu <william.wu.xudong@gmail.com>
2011-07-27 02:11:40 +0800

    overwrite the view method of lining sets
    Signed-off-by: Xudong Wu <william.wu.xudong@gmail.com>

--- app/controllers/lining_sets_controller.php
+++ app/controllers/lining_sets_controller.php
@@ -4,4 +4,16 @@ class LiningSetsController extends AppController {
         'Html', 'Xml');
     var $components = array(
         'RequestHandler', 'Session');
+    
+    function view($lining_id = null, $lining_fabric_id = null) {
+        $this->log($lining_id);
+        $this->log($lining_fabric_id);
+        if (null != $lining_id && null != $lining_fabric_id) {
+            $model = ClassRegistry::init($this->modelClass)->find('all', 
+                    array(
+                        'conditions' => array(
+                        'LiningSet.lining_id' => $lining_id, 'LiningSet.lining_fabric_id =' => $lining_fabric_id)));
+            $this->set('model', $model);
+        }
+    }
 }
