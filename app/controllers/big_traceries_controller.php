9dd4adcfc6e0e96e6659cb8d0d70aa301eda692e
Xudong Wu <william.wu.xudong@gmail.com>
2011-09-18 14:39:11 +0800

    remove unused code
    Signed-off-by: Xudong Wu <william.wu.xudong@gmail.com>

--- app/controllers/big_traceries_controller.php
+++ app/controllers/big_traceries_controller.php
@@ -3,20 +3,5 @@ class BigTraceriesController extends AppController {
     var $helpers = array(
         'Html', 'Xml');
     var $components = array(
-        'RequestHandler', 'Session');
-    
-    function view($category_id = null, $sub_category_id = null) {
-        if ($sub_category_id != null) {
-            $model = $this->SubCategory->find('all', 
-                    array(
-                        'conditions' => array(
-                        'SubCategory.category_id = ' => $category_id, 'SubCategory.id =' => $sub_category_id)));
-        } else {
-            $model = $this->SubCategory->find('all', 
-                    array(
-                        'conditions' => array(
-                        'SubCategory.category_id = ' => $category_id)));
-        }
-        $this->set('model', $model);
-    }
+        'RequestHandler', 'Session');    
 }
